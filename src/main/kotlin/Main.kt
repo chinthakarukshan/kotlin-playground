21e8e4aa2ad1cab5ed3cfb8a4801a8cfebb16f26
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2024-05-29 23:20:15 +0530

    Variable declaration methodologies

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,24 @@
 package org.example
 
 fun main() {
+    val varOne:Int; //Variable declaration without initialization
     val name="Mary"
     val age=20
 
     print("$name is $age years old.")
+
+
+    /*
+    * Explicit declaraton of variables.
+    * */
+    val a:Int = 1000
+    val b:String = "log message"
+    val c:Double = 3.14
+    val d:Long = 100_000_000_000_000
+    val e:Boolean = false
+    val f:Char = '\n'
+
+
+    varOne = 300 // Initializing the variable
+
 }
