3728a6cefbf06bc45339556cdabbfe0f15ae1389
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2024-06-12 15:22:31 +0530

    Class declaration and object creation

---
+++ src/main/kotlin/com/nygen/Contact.kt
@@ -0,0 +1,4 @@
+package org.example.com.nygen
+
+class Contact(val id: Int, val email: String) {
+}
---
+++ src/main/kotlin/com/nygen/KotlinTest.kt
@@ -0,0 +1,5 @@
+package org.example.com.nygen
+
+fun main() {
+    val contact = Contact(1, "tset@gmail.com")
+}
