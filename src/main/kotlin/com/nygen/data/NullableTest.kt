0c00a6ed48753556af633ce3ec53095412618469
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2024-06-24 18:36:19 +0530

    Default not null variable

---
+++ src/main/kotlin/com/nygen/data/NullableTest.kt
@@ -0,0 +1,9 @@
+package org.example.com.nygen.data
+
+fun main() {
+    var neverNull: String = "This cannot be null"
+
+    //This cannot never be null. Compilation error
+    //neverNull = null
+
+}
