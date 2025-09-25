13a5a209cb83e3886f6cb028d6590fcffc3e52b8
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-09-25 23:09:12 +0530

    Kotlin Loops

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,12 @@ package com.nygen.com
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("Pow!")
+    var x = 1
+    println("Before the loop x: $x")
+
+    while(x<4) {
+        println("In the loop x: $x")
+        x=x+1
+    }
+    println("After the loop x: $x")
 }
