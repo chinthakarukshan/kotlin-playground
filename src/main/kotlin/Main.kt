3b3c3ad6ecccce0230c86cdca263a96fecd90723
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-09-27 23:04:24 +0530

    using if as an expression

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,9 @@ package com.nygen.com
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var x = 1
-    println("Before the loop x: $x")
+    val x=3
+    val y=1
 
-    while(x<4) {
-        println("In the loop x: $x")
-        x=x+1
-    }
-    println("After the loop x: $x")
+    println(if(x>y) "x is greater than y" else "x is not greater than y")
+    println("This line runs no matter what")
 }
