5b91bae90001c743050b350114ad16b80a70af6e
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-10-19 23:31:26 +0530

    Improved the code to accept a valid user input

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,6 +1,29 @@
 fun main(args: Array<String>) {
     val options = arrayOf("Rock","Paper","Scissors")
     val gameChoice = getGameChoice(options)
+    val userChoice = getUserChoice(options)
 }
 
 fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random() * optionsParam.size).toInt()]
+
+fun getUserChoice(optionsParam: Array<String>):String {
+    var isValidInput = false
+    var usersChoice = ""
+
+    while(!isValidInput) {
+        print("Please enter one of the following: ")
+        for(item in optionsParam) print(" $item")
+        println(".")
+
+        val userInput = readLine()
+
+        if(!userInput.isNullOrEmpty() && optionsParam.contains(userInput)) {
+            isValidInput = true
+            usersChoice = userInput
+        } else {
+            println("You must enter a valid choice")
+        }
+    }
+
+    return usersChoice
+}
