f8ecc2f150a44b66db1030a540410e58969ed67c
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-11-06 22:26:05 +0530

    Array of objects

--- src/main/kotlin/com/nygen/test/Dog.kt
+++ src/main/kotlin/com/nygen/test/Dog.kt
@@ -2,6 +2,6 @@ package com.nygen.test
 
 class Dog(val name: String, var weight: Int, val breed: String) {
     fun bark() {
-        print(if (weight<20) "Yip!" else "Woof")
+        println(if (weight<20) "Yip!" else "Woof")
     }
 }
--- src/main/kotlin/com/nygen/test/TestClass.kt
+++ src/main/kotlin/com/nygen/test/TestClass.kt
@@ -5,4 +5,10 @@ fun main(args: Array<String>) {
     println(myDog.name)
     myDog.weight=50
     myDog.bark()
+
+    var dogs = arrayOf(Dog("Rocky",27,"Mixed"), Dog("Bunty",24,"Labrador"))
+    dogs[1].weight=15
+    dogs[1].bark()
+
+    dogs.forEach { it -> it.bark() }
 }
