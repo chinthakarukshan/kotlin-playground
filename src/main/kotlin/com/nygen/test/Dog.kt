0cd5ad128b9822e9e433fead242bc4a5cb35d081
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-11-06 22:20:11 +0530

    Assign new values to properties

--- src/main/kotlin/com/nygen/test/Dog.kt
+++ src/main/kotlin/com/nygen/test/Dog.kt
@@ -1,6 +1,6 @@
 package com.nygen.test
 
-class Dog(val name: String, val weight: Int, val breed: String) {
+class Dog(val name: String, var weight: Int, val breed: String) {
     fun bark() {
         print(if (weight<20) "Yip!" else "Woof")
     }
--- src/main/kotlin/com/nygen/test/TestClass.kt
+++ src/main/kotlin/com/nygen/test/TestClass.kt
@@ -2,4 +2,6 @@ package com.nygen.test
 
 fun main(args: Array<String>) {
     var myDog = Dog("Fido",70,"Mixed")
+    println(myDog.name)
+    myDog.weight=50
 }
