bd3a8f90c2905e9d9b928a8baccb1a6153553880
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-11-07 10:20:53 +0530

    Added drumkit class

---
+++ src/main/kotlin/com/nygen/drumkit/DrumKit.kt
@@ -0,0 +1,11 @@
+package com.nygen.drumkit
+
+class DrumKit(var hasTopHat: Boolean,var hasSnare: Boolean) {
+    fun playTopHat(){
+        if(hasTopHat) println("ding ding ba-da-bing!")
+    }
+
+    fun playSnare(){
+        if (hasSnare) println("bang bang bang!")
+    }
+}
