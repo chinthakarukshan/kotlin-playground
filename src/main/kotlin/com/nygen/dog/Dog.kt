1a057b805605b21b53efaa72392c1834935ca8e3
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-11-11 10:34:19 +0530

    Initializer blocks

--- src/main/kotlin/com/nygen/dog/Dog.kt
+++ src/main/kotlin/com/nygen/dog/Dog.kt
@@ -2,5 +2,14 @@ package com.nygen.dog
 
 class Dog(val name:String, var weight: Int,breed_param:String) {
 
+    init {
+        println("Dog constructor has run. But the breed property has not been initiated")
+        println(breed_param)
+    }
+
     val breed = breed_param.uppercase()
+
+    init {
+        println("Dog constructor has run. Breed property has initiated with the value of $breed")
+    }
 }
