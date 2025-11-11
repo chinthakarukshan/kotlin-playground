e68f437b260b97154917901bd2a447d8e2141bcf
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-11-11 10:31:23 +0530

    Property initialization in the class body

--- src/main/kotlin/com/nygen/dog/Dog.kt
+++ src/main/kotlin/com/nygen/dog/Dog.kt
@@ -1,4 +1,6 @@
 package com.nygen.dog
 
-class Dog(val name:String, var weight: Int,val breed:String) {
+class Dog(val name:String, var weight: Int,breed_param:String) {
+
+    val breed = breed_param.uppercase()
 }
