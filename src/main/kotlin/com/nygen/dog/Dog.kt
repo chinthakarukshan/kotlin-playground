eb9af1cd24d8a3de43f4cafd63088cf42254c1f4
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-11-15 23:31:03 +0530

    Added getter method for weightInKgs property

--- src/main/kotlin/com/nygen/dog/Dog.kt
+++ src/main/kotlin/com/nygen/dog/Dog.kt
@@ -2,6 +2,7 @@ package com.nygen.dog
 
 class Dog(val name:String, var weight: Int,breed_param:String) {
 
+    var activities = arrayOf("Walks")
     init {
         println("Dog constructor has run. But the breed property has not been initiated")
         println(breed_param)
@@ -12,4 +13,7 @@ class Dog(val name:String, var weight: Int,breed_param:String) {
     init {
         println("Dog constructor has run. Breed property has initiated with the value of $breed")
     }
+
+    val weightInKgs: Double
+        get() = weight/2.2  // Getter or accessor of weightInKgs property
 }
--- src/main/kotlin/com/nygen/dog/DogTest.kt
+++ src/main/kotlin/com/nygen/dog/DogTest.kt
@@ -2,4 +2,5 @@ package com.nygen.dog
 
 fun main(args: Array<String>) {
     val dog=Dog("Bunty",14,"mixed")
+    dog.weightInKgs
 }
