5b33d275c23a88e36366acf067cf1af2a196227f
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-11-22 00:00:40 +0530

    Initial implementation of animal class

--- src/main/kotlin/com/nygen/inherit/Animal.kt
+++ src/main/kotlin/com/nygen/inherit/Animal.kt
@@ -1,4 +1,24 @@
 package com.nygen.inherit
 
 class Animal {
+    val image = ""
+    val foot = ""
+    val habitat = ""
+    val hunger = 10
+
+    fun makeNoise() {
+        println("The animal is making a noise")
+    }
+
+    fun eat() {
+        println("The animal is eating")
+    }
+
+    fun roam() {
+        println("The animal is roaming")
+    }
+
+    fun sleep() {
+        println("The animal is sleeping")
+    }
 }
