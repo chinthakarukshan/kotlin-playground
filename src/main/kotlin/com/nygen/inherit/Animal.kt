68ee893156694eff1e5e63bc6f9e3ae7e892c39d
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-11-23 23:14:38 +0530

    Changing the methods to abstract because no useful implementation in Animal class exists

--- src/main/kotlin/com/nygen/inherit/Animal.kt
+++ src/main/kotlin/com/nygen/inherit/Animal.kt
@@ -6,13 +6,9 @@ abstract class Animal {
     abstract val habitat: String
     val hunger = 10
 
-    open fun makeNoise() {
-        println("The animal is making a noise")
-    }
+    abstract fun makeNoise()
 
-    open fun eat() {
-        println("The animal is eating")
-    }
+    abstract fun eat()
 
     open fun roam() {
         println("The animal is roaming")
