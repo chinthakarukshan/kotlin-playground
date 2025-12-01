d35b7e9927582d305e114ae5dc1653d4954b8321
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-12-01 23:39:47 +0530

    Objects equivalant or identity

--- src/main/kotlin/com/nygen/restaurant/RecipeTest.kt
+++ src/main/kotlin/com/nygen/restaurant/RecipeTest.kt
@@ -1,20 +1,22 @@
 package com.nygen.restaurant
 
-fun main(args: Array<String>){
-    var r1 = Recipe("Chicken Bhuna",false)
-    val r2 = Recipe("Chicken Bhuna",false)
+fun main(args: Array<String>) {
+    var r1 = Recipe("Chicken Bhuna", false)
+    val r2 = Recipe("Chicken Bhuna", false)
 
-    if(r1==r2) {
+    if (r1 == r2) {
         println("Two recipes are equal")
     }
     println(r1.toString())
     println(r2.toString())
 
-    var r3 = Recipe("Thai curry",false)
+    var r3 = Recipe("Thai curry", false)
     var r4 = r3.copy(isVegetarian = false)
 
     println(r3.component1())
 
-    val(title,isVegi) = r3
+    val (title, isVegi) = r3
     println("$title $isVegi")
+    println(if (r1 == r2) "Object equivalant" else "false")
+    println(if (r1 === r2) "Object identical" else "objects are not identical")
 }
