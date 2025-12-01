b35ee8c753e7ebfac868d201c400cf38022f2823
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-12-01 22:43:41 +0530

    Equality of two data objects

---
+++ src/main/kotlin/com/nygen/restaurant/RecipeTest.kt
@@ -0,0 +1,10 @@
+package com.nygen.restaurant
+
+fun main(args: Array<String>){
+    var r1 = Recipe("Chicken Bhuna",false)
+    val r2 = Recipe("Chicken Bhuna",false)
+
+    if(r1==r2) {
+        println("Two recipes are equal")
+    }
+}
