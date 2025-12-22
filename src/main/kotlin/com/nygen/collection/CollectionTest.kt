3b42238f4f9d41292b0df987ef7ee06e88eef0ff
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2025-12-22 10:41:45 +0530

    Checking the collection size and printing

--- src/main/kotlin/com/nygen/collection/CollectionTest.kt
+++ src/main/kotlin/com/nygen/collection/CollectionTest.kt
@@ -3,5 +3,7 @@ package com.nygen.collection
 fun main(args: Array<String>) {
     val shopping: List<String> = listOf("Tea","Eggs","Milk")
 
-    shopping.forEach { println(it) }
+    if (shopping.size > 0) {
+        println(shopping[0])
+    }
 }
