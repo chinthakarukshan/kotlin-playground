79eb158cb5645b3549264e016301f1ff9c220ef4
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2026-01-07 09:20:46 +0530

    Defined Map initialization

--- src/main/kotlin/com/nygen/collection/CollectionTest.kt
+++ src/main/kotlin/com/nygen/collection/CollectionTest.kt
@@ -81,4 +81,5 @@ fun main(args: Array<String>) {
     val receip03 = Recipe("Dhal tikka", true)
 
     val recipeMap = mapOf("Recipe01" to receip01, "Recipe02" to receip02, "Recipe03" to receip03)
+    val recipeMap02: Map<String, Recipe> = mapOf("Recipe01" to receip01, "Recipe02" to receip02, "Recipe03" to receip03)
 }
