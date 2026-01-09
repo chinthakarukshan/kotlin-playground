c94c59773fa300c461546507b4fabacf75b54f23
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2026-01-09 23:12:38 +0530

    Add score improvement

--- src/main/kotlin/com/nygen/pet/Contest.kt
+++ src/main/kotlin/com/nygen/pet/Contest.kt
@@ -2,4 +2,8 @@ package com.nygen.pet
 
 class Contest<T: Pet> {
     val scores: MutableMap<T, Int> = mutableMapOf()
+
+    fun addScore(t: T, score:Int) {
+        if (score >= 0) scores.put(t, score)
+    }
 }
