ba12b4826b8ccbba0ad08399e0b60744424e5229
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2026-01-18 22:18:47 +0530

    Covariant generics usage

--- src/main/kotlin/com/nygen/pet/PetTest.kt
+++ src/main/kotlin/com/nygen/pet/PetTest.kt
@@ -24,5 +24,6 @@ fun main(args: Array<String>) {
     val dogRetailer: Retailer<Dog> = DogRetailer()
     val catRetailer: Retailer<Cat> = CatRetailer()
 
-    val fishRetailer: Retailer<Pet> = FishRetailer()
+    val petRetailer: Retailer<Pet> = FishRetailer()
+    petRetailer.sell()
 }
