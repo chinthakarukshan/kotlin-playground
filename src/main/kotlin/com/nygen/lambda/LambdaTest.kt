ee44503275e22242844b3c90188376757adeb2b2
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2026-01-22 10:40:37 +0530

    Invoking the function without parantheses

--- src/main/kotlin/com/nygen/lambda/LambdaTest.kt
+++ src/main/kotlin/com/nygen/lambda/LambdaTest.kt
@@ -27,10 +27,18 @@ fun main(args: Array<String>) {
 
     convert(20.0,{c:Double -> c*1.8 +32})
     val farenhite = convert(20.0){c:Double -> c*1.8 +32}
+
+    convertFive { it *1.8 + 32 }
 }
 
 fun convert(x:Double, converter:(Double)->Double) : Double {
     val result = converter(x)
     println("$x is converted to $result")
+    return result
+}
+
+fun convertFive(converter:(Int) -> Double): Double {
+    val result = converter(5)
+    println("5 is converted to $result")
     return result
 }
