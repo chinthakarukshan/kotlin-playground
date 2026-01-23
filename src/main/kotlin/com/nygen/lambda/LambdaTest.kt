2b4fa4862d66bf989ae7e18e5b59e716f231083c
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2026-01-23 09:59:49 +0530

    Return lambda from a function

--- src/main/kotlin/com/nygen/lambda/LambdaTest.kt
+++ src/main/kotlin/com/nygen/lambda/LambdaTest.kt
@@ -29,6 +29,9 @@ fun main(args: Array<String>) {
     val farenhite = convert(20.0){c:Double -> c*1.8 +32}
 
     convertFive { it *1.8 + 32 }
+
+    val farValue = getConversionLambda("CentigradeToFehrenheit")(5.0)
+    println(farValue)
 }
 
 fun convert(x:Double, converter:(Double)->Double) : Double {
@@ -41,4 +44,16 @@ fun convertFive(converter:(Int) -> Double): Double {
     val result = converter(5)
     println("5 is converted to $result")
     return result
+}
+
+fun getConversionLambda(str: String): (Double) -> Double {
+    if (str=="CentigradeToFehrenheit") {
+        return { it* 1.8 + 32 }
+    } else if (str=="KgsToPounds") {
+        return { it * 2.204623}
+    } else if (str=="PoundsToUSTons") {
+        return {it/2000.0}
+    } else {
+        return {it}
+    }
 }
