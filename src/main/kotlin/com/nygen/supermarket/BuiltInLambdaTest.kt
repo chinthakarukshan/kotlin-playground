86f57ce9cccf18257a76f19acbb519111db232ab
Chinthaka Weerakkody <chinthakarukshan@gmail.com>
2026-01-31 23:18:13 +0530

    minBy lambda usage

--- src/main/kotlin/com/nygen/supermarket/BuiltInLambdaTest.kt
+++ src/main/kotlin/com/nygen/supermarket/BuiltInLambdaTest.kt
@@ -2,13 +2,16 @@ package com.nygen.supermarket
 
 fun main(args: Array<String>) {
     val groceries = listOf(
-        Grocery("Tomatoes", "Vegetable", "lb",3.0,1),
-        Grocery("Bagels", "Bakery", "Packets", 1.5,1),
-        Grocery("Olive oil", "Pantry", "Bottle", 6.0,1),
-        Grocery("Ice cream", "Frozen", "Pack", 3.0,1),
-        Grocery("Mushroom", "Vegetable", "lb",4.0,1)
+        Grocery("Tomatoes", "Vegetable", "lb",3.0,11),
+        Grocery("Bagels", "Bakery", "Packets", 1.5,5),
+        Grocery("Olive oil", "Pantry", "Bottle", 6.0,10),
+        Grocery("Ice cream", "Frozen", "Pack", 3.0,3),
+        Grocery("Mushroom", "Vegetable", "lb",4.0,2)
     )
 
     val highestUnitPrice = groceries.maxBy { it.unitPrice }
     println(highestUnitPrice)
+
+    val lowestQuantity = groceries.minBy { it.quantity }
+    println(lowestQuantity)
 }
